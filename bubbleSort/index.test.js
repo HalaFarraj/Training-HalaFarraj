3e9e628946fc463eff8fbe746a2435ddd3fb5359
Hala Farraj <hala@hala-farraj-pc.scoiumtech.local>
2022-11-29 11:19:39 +0300

    Challenge 5 - bubbleSort : solved with no credits as a first attempt

---
+++ bubbleSort/index.test.js
@@ -0,0 +1,66 @@
+/*
+ * Bubble sort is the most basic sorting algorithm in all of Computer
+ * Science dom. It works by starting at the first element of an array and
+ * comparing it to the second element; if the first element is greater than the
+ * second element, it swaps the two. It then compares the second to the third,
+ * and the third to the fourth, and so on; in this way, the largest values
+ * "bubble" to the end of the array. Once it gets to the end of the array, it
+ * starts over and repeats the process until the array is sorted numerically.
+ *
+ * Implement a function that takes an array and sorts it using this technique.
+ * Don't use JavaScript's built-in sorting function (Array.prototype.sort).
+ *
+ * QUERY: What's the time complexity of your algorithm? If you don't already
+ * know, try to intuit this without consulting the Googles.
+ *
+ * Extra credit: Optimization time! During any given pass, if no elements are
+ * swapped we can assume the list is sorted and can exit the function early.
+ * After this optimization, what is the time complexity of your algorithm?
+ *
+ * More credits: Do you need to consider every element every time you iterate
+ * through the array? Again: Has the time complexity of your algorithm changed?
+*/
+
+
+
+// O(n^2)
+const bubbleSort = (array) => {
+    for (let i = 0; i<array.length;i++)
+    {
+        for (let j=0; j<array.length-1;j++)
+        {
+            
+            if (array[j]>array[j+1])
+            {
+                let temp = array[j];
+                array[j]=array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+};
+
+
+//test cases 
+let r = [2, -1, -3];
+bubbleSort(r);
+console.log(r);
+
+let x = [2, 2, 2];
+bubbleSort(x);
+console.log(x);
+
+let arr = [2, 1, 3];
+bubbleSort(arr);
+console.log(arr);
+
+
+const a = [34, 203, 3, 746, 200, 984, 198, 764, 9];
+bubbleSort(a);
+console.log(a);
+
+
+/**
+ * Remember to look here http://visualgo.net/sorting
+ *
+*/
