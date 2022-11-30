93addf3d7352da69c39934c6ad0cfe54624fac5a
Hala Farraj <hala@hala-farraj-pc.scoiumtech.local>
2022-11-30 11:57:13 +0300

    Challenge 6 - buyAndSell : Approach2-> Enhanced time complextiy

--- buyAndSell/index.test.js
+++ buyAndSell/index.test.js
@@ -1,29 +1,30 @@
+// approach 2 : find the min number, then find the max difference between it and each element
+// Objective: Enhance the time complexity to be O(n)
 
+function getMinOfArray(numArray) {
+  return Math.min.apply(null, numArray);
+}
 
-//approach 1: exhaustive searching for the profit 
-// O(n^2)
+const maximumProfit = function(array){
+    // let minCost = Math.min.apply(Math,array);
 
-const maximumProfit = function(array) {
-    let maxProfit = 0;
-    for (let i=0 ; i<array.length;i++)
+    let minCost = getMinOfArray(array); // O(n)
+    let indexOfMinCost = array.indexOf(minCost);
+    let maxProfit =0;
+
+    for (let i = indexOfMinCost; i<array.length;i++)
     {
-      let j = array.length-1;
-
-      while(i<=j)
-      {
-        if (array[j] > array[i]){
-            let profit = array[j] - array[i];
-            if (profit > maxProfit)
-              maxProfit = profit;
-        }
-        j--;
-      }
+      if (array[i]-minCost > maxProfit)
+         maxProfit=array[i]-minCost ;
     }
 
     return maxProfit;
-};
+}
 
 
 //test cases 
-let a = [100, 80, 120, 130, 70, 60, 100, 125];
-console.log(maximumProfit(a))
+const a = [100, 80, 120, 130, 70, 60, 100, 125];
+console.log(maximumProfit(a));
+
+const b = [100, 80, 70, 65, 60, 55, 50];
+console.log(maximumProfit(b));
